305beae8f735e46d360e7d6fa07fb3feadaf1f5f
imtyaz-17 <imtyazit17017@gmail.com>
2024-07-29 23:15:32 +0600

    Admin->Category - Factory - functionality implemented

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -17,7 +17,9 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->name(),
+            'slug' => fake()->unique()->slug(),
+            'status' => fake()->boolean(),
         ];
     }
 }
