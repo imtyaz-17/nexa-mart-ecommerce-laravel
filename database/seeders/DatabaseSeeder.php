62bc9c2dff0e4e01be843549b42a317204deee31
imtyaz-17 <imtyazit17017@gmail.com>
2024-08-22 22:47:57 +0600

    fix: update.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,6 @@ class DatabaseSeeder extends Seeder
 //            'email' => 'test@example.com',
 //        ]);
 
-        Category::factory(30)->create();
+//        Category::factory(30)->create();
     }
 }
