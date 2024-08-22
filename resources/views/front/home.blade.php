900da0bdbefabea117a41d92b01799496ae6c024
imtyaz-17 <imtyazit17017@gmail.com>
2024-08-22 16:10:32 +0600

    fix: routes and carousel content.

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -9,36 +9,34 @@
                     <!-- <img src="images/carousel-1.jpg" class="d-block w-100" alt=""> -->
 
                     <picture>
-                        <source media="(max-width: 799px)"
-                                srcset="{{asset('frontend-assets/images/carousel-1-m.jpg')}}"/>
-                        <source media="(min-width: 800px)" srcset="{{asset('frontend-assets/images/carousel-1.jpg')}}"/>
-                        <img src="images/carousel-1.jpg" alt=""/>
+                        <source media="(max-width: 799px)" srcset="{{asset('frontend-assets/images/carousel-1-m.jpg')}}" />
+                        <source media="(min-width: 800px)" srcset="{{asset('frontend-assets/images/carousel-1.jpg')}}" />
+                        <img src="images/carousel-1.jpg" alt="" />
                     </picture>
 
                     <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                         <div class="p-3">
                             <h1 class="display-4 text-white mb-3">Kids Fashion</h1>
-                            <p class="mx-md-5 px-5">Lorem rebum magna amet lorem magna erat diam stet. Sadips duo stet
-                                amet amet ndiam elitr ipsum diam</p>
-                            <a class="btn btn-outline-light py-2 px-4 mt-3" href="#">Shop Now</a>
+                            <p class="mx-md-5 px-5">Explore our latest collection of stylish and comfortable kids' clothing.
+                                Perfect for every occasion, designed with love.</p>
+                            <a class="btn btn-outline-light py-2 px-4 mt-3" href="{{route('shop')}}">Shop Now</a>
                         </div>
                     </div>
                 </div>
                 <div class="carousel-item">
 
                     <picture>
-                        <source media="(max-width: 799px)"
-                                srcset="{{asset('frontend-assets/images/carousel-2-m.jpg')}}"/>
-                        <source media="(min-width: 800px)" srcset="{{asset('frontend-assets/images/carousel-2.jpg')}}"/>
-                        <img src="{{asset('frontend-assets/images/carousel-2.jpg')}}" alt=""/>
+                        <source media="(max-width: 799px)" srcset="{{asset('frontend-assets/images/carousel-2-m.jpg')}}" />
+                        <source media="(min-width: 800px)" srcset="{{asset('frontend-assets/images/carousel-2.jpg')}}" />
+                        <img src="{{asset('frontend-assets/images/carousel-2.jpg')}}" alt="" />
                     </picture>
 
                     <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                         <div class="p-3">
                             <h1 class="display-4 text-white mb-3">Womens Fashion</h1>
-                            <p class="mx-md-5 px-5">Lorem rebum magna amet lorem magna erat diam stet. Sadips duo stet
-                                amet amet ndiam elitr ipsum diam</p>
-                            <a class="btn btn-outline-light py-2 px-4 mt-3" href="#">Shop Now</a>
+                            <p class="mx-md-5 px-5">Discover the latest trends in women's fashion. From chic dresses to
+                                casual wear, find the perfect outfit for any occasion.</p>
+                            <a class="btn btn-outline-light py-2 px-4 mt-3" href="{{route('shop')}}">Shop Now</a>
                         </div>
                     </div>
                 </div>
@@ -46,18 +44,17 @@
                     <!-- <img src="images/carousel-3.jpg" class="d-block w-100" alt=""> -->
 
                     <picture>
-                        <source media="(max-width: 799px)"
-                                srcset="{{asset('frontend-assets/images/carousel-3-m.jpg')}}"/>
-                        <source media="(min-width: 800px)" srcset="{{asset('frontend-assets/images/carousel-3.jpg')}}"/>
-                        <img src="{{asset('frontend-assets/images/carousel-2.jpg')}}" alt=""/>
+                        <source media="(max-width: 799px)" srcset="{{asset('frontend-assets/images/carousel-3-m.jpg')}}" />
+                        <source media="(min-width: 800px)" srcset="{{asset('frontend-assets/images/carousel-3.jpg')}}" />
+                        <img src="{{asset('frontend-assets/images/carousel-2.jpg')}}" alt="" />
                     </picture>
 
                     <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
                         <div class="p-3">
                             <h1 class="display-4 text-white mb-3">Shop Online at Flat 70% off on Branded Clothes</h1>
-                            <p class="mx-md-5 px-5">Lorem rebum magna amet lorem magna erat diam stet. Sadips duo stet
-                                amet amet ndiam elitr ipsum diam</p>
-                            <a class="btn btn-outline-light py-2 px-4 mt-3" href="#">Shop Now</a>
+                            <p class="mx-md-5 px-5">Enjoy massive discounts on top brands. Shop now and save big on your
+                                favorite fashion items!</p>
+                            <a class="btn btn-outline-light py-2 px-4 mt-3" href="{{route('shop')}}">Shop Now</a>
                         </div>
                     </div>
                 </div>
