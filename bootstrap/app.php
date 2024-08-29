70300020475b30d8c0d57b9de50d9248b7a413fe
imtyaz-17 <imtyazit17017@gmail.com>
2024-08-29 21:08:58 +0600

    fix: auth alias change

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,7 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         //
         $middleware->alias([
-            'admin.auth' => \App\Http\Middleware\AdminAuthenticate::class
+            'isAdmin' => \App\Http\Middleware\AdminAuthenticate::class
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
